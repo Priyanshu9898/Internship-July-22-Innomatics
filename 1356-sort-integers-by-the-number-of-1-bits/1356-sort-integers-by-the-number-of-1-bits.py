2c92cfb6d9fce0f3ef7cff477fd220e38a0eb839
Priyanshu Malaviya <87270273+Priyanshu9898@users.noreply.github.com>
2022-08-01 21:35:17 +0530

    Delete 1356-sort-integers-by-the-number-of-1-bits directory

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def sortByBits(self, arr: List[int]) -> List[int]:
-        s=[[bin(arr[x]).replace("0b","").count('1'),arr[x]]  for x in range(0,len(arr))]
-        
-        s.sort()
-        #print(s)
-        return [ x[1] for x in s]
